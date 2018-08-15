f73644878f74fac40f6b99e06097a407eaa2f376
jisung <jisung.smart@gmail.com>
2018-08-16 06:08:14 +0900

    Add collectionVie outlet
    To implement mathch app I added collectionView and add related delegate in the Vviewcontroller.swift file. and show card on screen add three function in the class.

--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -8,23 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var model = CardModel()
     var cardArray = [Card]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Call the get card method of the card model
-       cardArray =  model.getCard()
         
+        //Call the get card method of the card model
+        cardArray =  model.getCard()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return cardArray.count
+    
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    }
 }
 
 
