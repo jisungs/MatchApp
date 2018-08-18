b94ffb2259a1a859cdd051beaf3fc4167dff37f4
jisung <jisung.smart@gmail.com>
2018-08-18 11:19:03 +0900

    Add logic that match card
    When the collectionView scrolled down the card that matched are still apear on the screen or not it' randomly worked. so reload the cell if the condition is match true.

--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     var model = CardModel()
     var cardArray = [Card]()
+    
+    var firstFlippedCardIndex:IndexPath?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,7 +64,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //Get the card that user selected
         let card = cardArray[indexPath.row]
         
-        if card.isFlipped == false {
+        if card.isFlipped == false && card.isMatched == false {
             
             //flip the card
             cell.flip()
@@ -70,17 +72,69 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             //Set the status of the card
             card.isFlipped = true
             
-        }else {
-            //Flip the card back
-            cell.flipBack()
+            //Determine if it's the first card or second card that's flipped over.
+            if firstFlippedCardIndex == nil {
+                //This is the first card being flipped
+                firstFlippedCardIndex = indexPath
+                
+            } else {
+                //This is the second card being flipped
+                
+                // Perform the matching logic
+                checkForMatches(indexPath)
+                
+            }
             
-            //Set the status of the card
-            card.isFlipped = false
         }
         
+    }// Ending of didSelectItemAt method
+    
+    //MARK : - Game Logic Method
+    func checkForMatches(_ secondFlippedCardIndex:IndexPath){
         
+        //Get the cells for the two cards were revealed
+        let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
         
+        let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as? CardCollectionViewCell
+        
+        // Get the card for the two cards were revealed
+        let cardOne = cardArray[firstFlippedCardIndex!.row]
+        let cardTwo = cardArray[secondFlippedCardIndex.row]
+        
+        //Compare the two cards
+       if cardOne.imageName == cardTwo.imageName {
+            // It's a match
+            
+            //Set the statuses of the cards
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            //Remove the card of the gird
+            cardOneCell?.remove()
+            cardTwoCell?.remove()
+            
+       } else {
+            //it's not a match
+        
+            //Set the status of the card
+            cardOne.isFlipped = false
+            cardTwo.isFlipped = false
+        
+            //Flip back both cards
+            cardOneCell?.flipBack()
+            cardTwoCell?.flipBack()
+        
+        }
+        //Tell the collectionview to reload the cell of the first card if it is nill
+        if cardOneCell == nil {
+            collectionView.reloadItems(at: [firstFlippedCardIndex!])
+        }
+        
+        //Reset the property that tracks the first card flipped
+        firstFlippedCardIndex = nil
     }
-}
+    
+    
+}//Ending of VeiwControllerCalss
 
 
